d999d6a41020a0808c22484bd57e0684aacadda7
oroibala <inphysicsitrust@gmail.com>
2021-10-15 22:07:18 +0600

    update

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,14 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking for a question {question_id}')
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse(f'You are looking for the results of question {question_id}')
@@ -13,5 +18,5 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')
-    output = '<hr>'.join([q.question_text for q in latest_question_list])
-    return HttpResponse(f'{output}')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
